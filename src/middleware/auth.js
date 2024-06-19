3e7fd4e8e13275dd86d81e2f4e186f0ae713bec6
AndresAndrada <andresandrada1994@gmail.com>
2024-06-18 23:20:16 -0300

    Add JWT

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,7 +2,6 @@ const { verifyToken } = require('../handler/verifyToken');
 
 const checkAuth = async (req, res, next) => {
     try {
-        console.log(req?.headers, 'ENTREEE');
         const token = req?.headers?.authorization ? req?.headers?.authorization.split(' ').pop() : undefined
         const tokenDAta = token ? await verifyToken(token) : undefined;
         console.log(tokenDAta, 'Token data');
